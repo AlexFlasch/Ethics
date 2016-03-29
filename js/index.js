b918fdc001a94fc344f34c40874c47c8f478299a
Flascher <alexanderflasch@gmail.com>
2016-03-29 00:54:12 -0500

    Finished website!

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    
+    $('#choice-a').click(function() {
+        window.location.href = 'choice-a.html';
+    });
+
+    $('#choice-b').click(function() {
+        window.location.href = 'choice-b.html';
+    });
+
+    $('#choice-c').click(function() {
+        window.location.href = 'choice-c.html';
+    });
 });
